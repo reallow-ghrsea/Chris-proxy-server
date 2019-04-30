4f178a7a264e014eac396b4e117b7f63193c358e
Chris Broveleit <Chris.Broveleit@gmail.com>
2019-04-29 19:32:37 -0700

    initial testing complete

--- server.js
+++ server.js
@@ -1,23 +1,33 @@
+require('newrelic');
 const express = require('express');
 const path = require('path');
-const morgan = require('morgan');
+const axios = require ('axios');
+const cors = require('cors');
+
+var service1Url= 'http://localhost:3333/api/listingGallery';
 
 const app = express();
+
 const port = process.env.PORT || 9000;
 
-app.use(morgan('dev'));
-console.log(__dirname);
-app.use(express.static(path.resolve(__dirname, 'public')));
+app.use('/:propertyId', express.static('public'));
+
+app.get('/api/listingGallery/:propertyId', cors(), (req, res)=> {
+  axios.get(service1Url+`/${req.params.propertyId}`)
+  .then( data => {
+    res.send(data.data);
+  })
+  .catch(e => {
+    console.log(e);
+  })
+})
+
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
-app.get('/:houseId', (req, res) => {
-  // let houseId = req.params.houseId;
 
-  res.sendFile(path.resolve(__dirname + '/public/index.html'));
-});
 app.listen(port, () => {
   console.log(`Proxy Server running at port ${port}`)
 });
